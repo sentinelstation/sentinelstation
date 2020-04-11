4b66727d2c7ddb8f141042c38b0608de640d0e43
Hex <hecksadecimal@outlook.com>
2020-04-11 19:11:12 -0400

    Less log spam

--- Tools/travis/ci/build.sh
+++ Tools/travis/ci/build.sh
@@ -3,10 +3,11 @@
 set -e
 set -x
 
-anywait(){
-    while kill -0 "$1"; do
+unitywait(){
+    sleep 60
+    while ps -C Unity >/dev/null; do
         echo "Building..."
-        sleep 1
+        sleep 120
     done
 }
 
@@ -15,9 +16,7 @@ echo "Building for $BUILD_TARGET"
 export BUILD_PATH=./Builds/$BUILD_TARGET/
 mkdir -p $BUILD_PATH
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} -projectPath $(pwd)/UnityProject -quit -batchmode -buildTarget $BUILD_TARGET -customBuildTarget $BUILD_TARGET -customBuildName $BUILD_NAME -customBuildPath $BUILD_PATH -executeMethod BuildCommand.PerformBuild & UNITYPID=$!
-
-anywait $UNITYPID
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} -projectPath $(pwd)/UnityProject -quit -batchmode -buildTarget $BUILD_TARGET -customBuildTarget $BUILD_TARGET -customBuildName $BUILD_NAME -customBuildPath $BUILD_PATH -executeMethod BuildCommand.PerformBuild & unitywait
 
 UNITY_EXIT_CODE=$?
 
